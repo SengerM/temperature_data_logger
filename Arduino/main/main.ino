528b1477ee387921ef01c29a21542bb4c573b207
SengerM <m.senger@hotmail.com>
2018-08-18 11:36:09 -0300

    Backup

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -7,7 +7,7 @@
 #define SENSORS_PIN 9
 #define N_SENSORS 4
 #define SD_BUSSY_LED_PIN 8
-#define FILENAME "DATA.TXT" // Max 8 chars of name + 3 of extension!!!
+#define DATA_FILE_NAME "datalog.txt" // Max 8 chars of name + 3 of extension!!!
 
 OneWire oneWireObject(SENSORS_PIN);
 DallasTemperature sensorDS18B20(&oneWireObject);
@@ -44,68 +44,71 @@ void loop() {
 	int k;
 	int current_day = -1;
 	DateTime now;
+	File dataFile;
 	
-	// Take measurements ------------------------------------
-	sensorDS18B20.requestTemperatures();
-    now = rtc.now();
-    // Create the data file ------------
-    SD_indicate_busy(1);
-	if (!SD.exists(FILENAME)) {
-		Serial.print("Creating file... ");
-		File dataFile = SD.open(FILENAME, FILE_WRITE);
-		if (dataFile) { // if the file is available, write to it:
-			dataFile.println("Year\tMonth\tDay\tHour\tMinute\tSecond\tTemperatures (C)");
+	while (1) {
+		// Take measurements ------------------------------------
+		sensorDS18B20.requestTemperatures();
+		now = rtc.now();
+		// Create the data file ------------
+		//~ SD_indicate_busy(1);
+		//~ if (!SD.exists(DATA_FILE_NAME)) {
+			//~ Serial.print("Creating file... ");
+			//~ dataFile = SD.open(DATA_FILE_NAME, FILE_WRITE);
+			//~ if (dataFile) { // If the file is available, write to it:
+				//~ dataFile.println("Year\tMonth\tDay\tHour\tMinute\tSecond\tTemperatures (C)");
+				//~ dataFile.close();
+				//~ SD_indicate_busy(0);
+				//~ Serial.println("File has been created");
+			//~ } else { // If the file isn't open, pop up an error:
+				//~ Serial.println("ERROR: Cannot open file in SD card");
+				//~ SD_indicate_busy(0);
+				//~ while (1);
+			//~ }
+		//~ }
+		// Write data to file -----------------------------------
+		SD_indicate_busy(1);
+		dataFile = SD.open(DATA_FILE_NAME, FILE_WRITE);
+		if (dataFile) { // If the file is available, write to it:
+			dataFile.print(now.year());
+			dataFile.print('\t');
+			dataFile.print(now.month());
+			dataFile.print('\t');
+			dataFile.print(now.day());
+			dataFile.print('\t');
+			dataFile.print(now.hour());
+			dataFile.print('\t');
+			dataFile.print(now.minute());
+			dataFile.print('\t');
+			dataFile.print(now.second());
+			dataFile.print('\t');
+			for (k=0; k<=N_SENSORS-1; k++) {
+				dataFile.print(sensorDS18B20.getTempCByIndex(k));
+				dataFile.print('\t');
+			}
+			dataFile.print('\n');
 			dataFile.close();
 			SD_indicate_busy(0);
-			Serial.println("File has been created");
+			
+			Serial.print("Data has been saved! ");
+			Serial.print(now.year());
+			Serial.print(" ");
+			Serial.print(now.month());
+			Serial.print(" ");
+			Serial.print(now.day());
+			Serial.print(" ");
+			Serial.print(now.hour());
+			Serial.print(" ");
+			Serial.print(now.minute());
+			Serial.print(" ");
+			Serial.println(now.second());
 		} else { // if the file isn't open, pop up an error:
 			Serial.println("ERROR: Cannot open file in SD card");
 			SD_indicate_busy(0);
 			while (1);
 		}
+		delay(1000); 
 	}
-    // Write data to file -----------------------------------
-    SD_indicate_busy(1);
-    File dataFile = SD.open(FILENAME, FILE_WRITE);
-	if (dataFile) { // if the file is available, write to it:
-		dataFile.print(now.year());
-		dataFile.print('\t');
-		dataFile.print(now.month());
-		dataFile.print('\t');
-		dataFile.print(now.day());
-		dataFile.print('\t');
-		dataFile.print(now.hour());
-		dataFile.print('\t');
-		dataFile.print(now.minute());
-		dataFile.print('\t');
-		dataFile.print(now.second());
-		dataFile.print('\t');
-		for (k=0; k<=N_SENSORS-1; k++) {
-			dataFile.print(sensorDS18B20.getTempCByIndex(k));
-			dataFile.print('\t');
-		}
-		dataFile.print('\n');
-		dataFile.close();
-		SD_indicate_busy(0);
-		
-		Serial.print("Data has been saved! ");
-		Serial.print(now.year());
-		Serial.print(" ");
-		Serial.print(now.month());
-		Serial.print(" ");
-		Serial.print(now.day());
-		Serial.print(" ");
-		Serial.print(now.hour());
-		Serial.print(" ");
-		Serial.print(now.minute());
-		Serial.print(" ");
-		Serial.println(now.second());
-	}
-	else { // if the file isn't open, pop up an error:
-		Serial.println("ERROR: Cannot open file in SD card");
-		while (1);
-	}
-	delay(1000); 
 }
 
 void SD_indicate_busy(int i) {
