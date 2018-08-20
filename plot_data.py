b7311d69b66a614fa1e6bc446c377e2b75eeb313
SengerM <m.senger@hotmail.com>
2018-08-20 11:22:58 -0300

    Agregué las bandas de error al gráfico.

--- plot_data.py
+++ plot_data.py
@@ -2,6 +2,8 @@ import numpy as np
 import datetime as dt
 import matplotlib.pyplot as plt
 import utils.my_plot_lib as mplt
+import uncertainties as unc
+from uncertainties import unumpy as unp
 
 figs = [] # Do not touch this.
 
@@ -13,13 +15,15 @@ for k in range(len(data)):
 	time[k] = dt.datetime(year=int(data[k][0]), month=int(data[k][1]), day=int(data[k][2]), hour=int(data[k][3]), minute=int(data[k][4]), second=int(data[k][5]))
 	temperatures[k] = data[k][6:].tolist()
 temperatures = list(map(list, zip(*temperatures))) # Transpose list.
+for k in range(len(temperatures)):
+	temperatures[k] = unp.uarray(temperatures[k], np.zeros(len(temperatures[k])) + 0.5) # This is the uncertainty in the datasheet https://datasheets.maximintegrated.com/en/ds/DS18B20.pdf
 # Plot data -----------------------
 f, axes = plt.subplots(1, sharex=True, figsize=(mplt.fig_width*mplt.fig_ratio[0]/25.4e-3, mplt.fig_width*mplt.fig_ratio[1]/25.4e-3)) # Create the figure for plotting.
 figs.append(f)
 f.subplots_adjust(hspace=0.3) # Fine-tune figure; make subplots close to each other and hide x ticks for all but bottom plot.
 f.suptitle('Temperaturas en la galería')
 for k in range(len(temperatures)):
-	axes.plot(time, temperatures[k], color=mplt.colors[k], label=r'$T_{:d}$'.format(k+1))
+	mplt.plot_yerr_filled(time, temperatures[k], axes, color=mplt.colors[k], label=r'$T_{:d}$'.format(k+1))
 mplt.beauty_grid(axes)
 axes.legend()
 axes.set_xlabel('Fecha')
